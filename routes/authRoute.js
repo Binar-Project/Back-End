78cd36d6819338406519c3cd9138a076dc6a6d3c
Achmad Syarif Wibisono <achmadsyarif000@gmail.com>
2023-06-18 21:29:15 +0700

    7

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const authController = require("../controllers/authController");
-const { verify } = require("argon2");
 
 const router = express.Router();
 
 router.post("/login", authController.Login);
-router.get("/dashboard", verify, authController.Dashboard);
-router.delete("/logout", verify, authController.Logout);
+router.get("/dashboard", authController.Dashboard);
+router.delete("/logout", authController.Logout);
 
 module.exports = router;
