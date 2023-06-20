4e53fe0229394154f08c6cd5ac621a1b634fb059
Achmad Syarif Wibisono <achmadsyarif000@gmail.com>
2023-06-20 13:51:10 +0700

    25

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const authController = require("../controllers/authController");
+const verifyUser = require("../middleware/authUser");
 
 const router = express.Router();
 
 router.post("/login", authController.Login);
-router.get("/dashboard", authController.Dashboard);
+router.get("/dashboard", verifyUser, authController.Dashboard);
 router.delete("/logout", authController.Logout);
 
 module.exports = router;
