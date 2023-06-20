6eef5f1ef906a555c63892a12ee57c31b4cbafa0
Achmad Syarif Wibisono <achmadsyarif000@gmail.com>
2023-06-20 15:14:08 +0700

    try catch mdw fix 2

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -1,49 +1,35 @@
 const { User } = require("../config/model/index");
 
 const verifyUser = async (req, res, next) => {
-  try {
-    if (!req.session.userId) {
-      return res.status(401).json({ message: "Mohon Login ke akun anda" });
-    }
-    const user = await User.findOne({
-      where: {
-        id_user: req.session.userId,
-      },
-    });
-    if (!user) {
-      return res.status(404).json({ message: "User tidak ditemukan" });
-    }
-    req.userId = user.id_user;
-    req.role = user.role;
-    next();
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ message: "Terjadi kesalahan dalam verifikasi pengguna" });
+  if (!req.session.userId) {
+    return res.status(401).json({ message: "Mohon Login ke akun anda" });
   }
+  const user = await User.findOne({
+    where: {
+      id_user: req.session.userId,
+    },
+  });
+  if (!user) {
+    return res.status(404).json({ message: "User tidak ditemukan" });
+  }
+  req.userId = user.id_user;
+  req.role = user.role;
+  next();
 };
 
 const verifyAdmin = async (req, res, next) => {
-  try {
-    const user = await User.findOne({
-      where: {
-        id_user: req.session.userId,
-      },
-    });
-    if (!user) {
-      return res.status(404).json({ message: "User tidak ditemukan" });
-    }
-    if (user.role !== "admin") {
-      return res.status(403).json({ message: "Anda tidak memiliki akses" });
-    }
-    next();
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ message: "Terjadi kesalahan dalam verifikasi pengguna" });
+  const user = await User.findOne({
+    where: {
+      id_user: req.session.userId,
+    },
+  });
+  if (!user) {
+    return res.status(404).json({ message: "User tidak ditemukan" });
   }
+  if (user.role !== "admin") {
+    return res.status(403).json({ message: "Anda tidak memiliki akses" });
+  }
+  next();
 };
 
-module.exports = { verifyUser, verifyAdmin };
+(module.exports = verifyUser), verifyAdmin;
