b6437d029889ca98ba5809789f181b08807f73a4
Achmad Syarif Wibisono <achmadsyarif000@gmail.com>
2023-06-22 19:18:37 +0700

    fix ventus.com

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(morgan("dev"));
 app.use(
   cors({
     credentials: true,
-    origin: ["https://ventus.achmadsyarif.com/", "https://react.achmadsyarif.com"],
+    origin: ["https://ventus.achmadsyarif.com", "https://react.achmadsyarif.com"],
   })
 );
 
