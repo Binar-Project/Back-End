a1601e621ed79f5060f7677c3fd29b322e7b723c
Achmad Syarif Wibisono <achmadsyarif000@gmail.com>
2023-06-22 19:58:32 +0700

    add vent-us.site

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(morgan("dev"));
 app.use(
   cors({
     credentials: true,
-    origin: ["https://ventus.achmadsyarif.com", "https://react.achmadsyarif.com"],
+    origin: ["http://vent-us.com", "https://react.achmadsyarif.com"],
   })
 );
 
